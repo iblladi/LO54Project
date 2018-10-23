44f7ccb68d4ba30e67a98e89fb3b851b3c6113ad
iblladi <ismail.blladi@utbm.fr>
2018-10-23 15:34:04 +0200

    New Project LO54

---
+++ src/main/java/fr/utbm/lo54/coursesession/CoursesessionApplication.java
@@ -0,0 +1,12 @@
+package fr.utbm.lo54.coursesession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesessionApplication.class, args);
+    }
+}
---
+++ src/test/java/fr/utbm/lo54/coursesession/CoursesessionApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.utbm.lo54.coursesession;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoursesessionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
