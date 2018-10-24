72d0042aee42084169a3c22e0c03b19040a3cf43
iblladi <ismail.blladi@utbm.fr>
2018-10-24 20:23:46 +0200

    Update src/main/java/fr/utbm/lo54/coursesession/entity/Course.java

--- src/main/java/fr/utbm/lo54/coursesession/entity/Course.java
+++ src/main/java/fr/utbm/lo54/coursesession/entity/Course.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.Objects;
 
 @Entity
 public class Course implements Serializable {
@@ -24,5 +25,45 @@ public class Course implements Serializable {
     public Course() {
     }
 
+    public Course(String title) {
+        this.title = title;
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(id, course.id) &&
+                Objects.equals(title, course.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
